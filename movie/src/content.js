91f897642b07b98d313271dc17eaaa31c6c0eaa5
IBCDuc <duc.dn242005@gmail.com>
2024-09-21 16:56:11 +0700

    using usereducer to create tdo

--- movie/src/content.js
+++ movie/src/content.js
@@ -1,51 +1,92 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 
 
+
+
+const init = {
+    job: '',
+    jobs: [],
+}
+const SET_JOB = 'set job'
+const ADD_JOB = 'add job'
+const DeLETE_JOB = 'delete job'
+const ADD_METHOD = payload => {
+    return {
+        type: ADD_JOB,
+        payload
+    }
+}
+const SET_METHOD = payload => {
+    return {
+        type: SET_JOB,
+        payload
+    }
+}
+
+const DELETE_METHOD = payload => {
+    return {
+        type: DeLETE_JOB,
+        index: payload
+    }
+
+}
+const reducer = (state, method) => {
+    let newState
+    switch (method.type) {
+        case SET_JOB:
+            newState =  {
+                ...state,
+                job: method.payload
+            }
+        break
+        case ADD_JOB:
+            newState = {
+                job: '',
+                jobs: [...state.jobs, state.job]
+            }
+        break
+        case DeLETE_JOB:
+            const newjob = [...state.jobs]
+            newjob.splice(method.index,1)
+            newState = {
+                ...state,
+                jobs: [newjob]
+            }
+        break
+    }
+
+
+
+
+    return newState
+}
 function Component() {
-
-    const course = [
-        {
-            id: 1,
-            content: 'Khoa hoc 1'
-        },
-        {
-            id: 2,
-            content: 'Khoa hoc 2'
-        },
-        {
-            id: 3,
-            content: 'Khoa hoc 3'
-        }
-    ]
-
-    const [high, sethigh] = useState(1)
-    
-    useEffect(() => {
-        const handlerComment = ( {detail} ) => {
-            console.log(detail)
-        }
-        
-        window.addEventListener(`lession-${high}`, handlerComment )
-        return () => {
-            window.removeEventListener(`lession-${high}`, handlerComment)
-        }
-        
-    }, [high])
-        
-
-
+    const [state, dispatch] = useReducer(reducer, init)
+    const { job, jobs } = state
+    const handleClick = () => {
+        dispatch(ADD_METHOD(job))
+    }
     return (
         <div>
-            {course.map((id) => (
-                <li
-                    style={{color: high === id.id ?
-                        'red':
-                        'black'
-                    }}
-                    onClick = {() => sethigh(id.id)}
-                    key = {id.id}
-                >{id.content}</li>
-            ))}
+            <input
+                value={job}
+                onChange={(e) => {
+                    dispatch(SET_METHOD(e.target.value))
+                }}
+            
+            ></input>
+
+            <button 
+                onClick = {handleClick}
+            
+            >Add</button>
+            <ul>
+                {jobs.map((item, index) => (
+                    <li key = {index}>{item} <span onClick = {() => dispatch(DELETE_METHOD(index))}>xoa</span></li>
+                
+                ))}
+            </ul>
+
         </div>
     )
 
